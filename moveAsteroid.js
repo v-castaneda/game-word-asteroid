a80e88c48e0b9bddedd41d279f3256785331bc6c
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-07-23 01:48:34 -0400

    updating move asteroid

---
+++ moveAsteroid.js
@@ -0,0 +1,22 @@
+// const asteroid = document.getElementById("asteroid");
+// asteroid.addEventListener("animationiteration", () => {
+//   let random = Math.floor(Math.random() * 3);
+//   left = random * 100;
+//   asteroid.style.left = left + "px";
+// });
+
+// setInterval(function () {
+//   let ssLeft = parseInt(
+//     window.getComputedStyle(spaceship).getPropertyValue("left")
+//   );
+//   let asteroidLeft = parseInt(
+//     window.getComputedStyle(asteroid).getPropertyValue("left")
+//   );
+//   let asteroidTop = parseInt(
+//     window.getComputedStyle(asteroid).getPropertyValue("top")
+//   );
+//   if (ssLeft === asteroidLeft && asteroidTop < 500 && asteroidTop > 300) {
+//     alert("Game Over");
+//     asteroid.style.animation = "none";
+//   }
+// }, 1);
--- newAsteroid.js
+++ newAsteroid.js
@@ -21,25 +21,3 @@ function newAsteroid(filePath) {
 
 const asteroid = document.getElementById("asteroid");
 console.log(asteroid);
-
-// block.addEventListener("animationiteration", () => {
-//   let random = Math.floor(Math.random() * 3);
-//   left = random * 100;
-//   block.style.left = left + "px";
-// });
-
-// setInterval(function () {
-//   let ssLeft = parseInt(
-//     window.getComputedStyle(spaceship).getPropertyValue("left")
-//   );
-//   let asteroidLeft = parseInt(
-//     window.getComputedStyle(asteroid).getPropertyValue("left")
-//   );
-//   let asteroidTop = parseInt(
-//     window.getComputedStyle(asteroid).getPropertyValue("top")
-//   );
-//   if (ssLeft === asteroidLeft && asteroidTop < 500 && asteroidTop > 300) {
-//     alert("Game Over");
-//     asteroid.style.animation = "none";
-//   }
-// }, 1);
