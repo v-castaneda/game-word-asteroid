0125be3a103a68f60d90114c34e4a4bbc4c3c121
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-07-23 01:45:07 -0400

    updating with new images

--- newAsteroid.js
+++ newAsteroid.js
@@ -18,3 +18,28 @@ function newAsteroid(filePath) {
 
   document.getElementById("game").appendChild(div);
 }
+
+const asteroid = document.getElementById("asteroid");
+console.log(asteroid);
+
+// block.addEventListener("animationiteration", () => {
+//   let random = Math.floor(Math.random() * 3);
+//   left = random * 100;
+//   block.style.left = left + "px";
+// });
+
+// setInterval(function () {
+//   let ssLeft = parseInt(
+//     window.getComputedStyle(spaceship).getPropertyValue("left")
+//   );
+//   let asteroidLeft = parseInt(
+//     window.getComputedStyle(asteroid).getPropertyValue("left")
+//   );
+//   let asteroidTop = parseInt(
+//     window.getComputedStyle(asteroid).getPropertyValue("top")
+//   );
+//   if (ssLeft === asteroidLeft && asteroidTop < 500 && asteroidTop > 300) {
+//     alert("Game Over");
+//     asteroid.style.animation = "none";
+//   }
+// }, 1);
