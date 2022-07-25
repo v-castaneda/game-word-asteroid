a767fd3a1bb9a0d5de93893dad9c4899773b220a
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-07-25 15:29:05 -0400

    adding clock functionality

--- index.js
+++ index.js
@@ -46,3 +46,27 @@ async function makeItRain() {
   await asteroid10.drop(1750);
 }
 makeItRain();
+
+let clockDiv = document.getElementById("clock");
+let startTime = 5;
+
+const countDown = setInterval(() => {
+  startTime--;
+  displayTime(startTime);
+  if ((startTime <= 0) | (startTime < 1)) {
+    endTime();
+    clearInterval(countDown);
+  }
+}, 1000);
+
+function displayTime(second) {
+  const min = Math.floor(second / 60);
+  const sec = Math.floor(second % 60);
+  clockDiv.innerHTML = `Time: ${min < 10 ? "0" : ""}${min}:${
+    sec < 10 ? "0" : ""
+  }${sec}`;
+}
+
+function endTime() {
+  clockDiv.innerHTML = "Level up";
+}
