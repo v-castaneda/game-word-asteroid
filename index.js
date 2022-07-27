00ad3af464af09e3e4a053be4366dbc3a1c7e75e
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-07-27 14:34:42 -0400

    updating comments

--- index.js
+++ index.js
@@ -23,53 +23,53 @@ images.sort((a, b) => {
 
 // creating asteroids
 const asteroid1 = new Asteroid(images[0]);
-const asteroid2 = new Asteroid(images[1]);
-const asteroid3 = new Asteroid(images[2]);
-const asteroid4 = new Asteroid(images[3]);
-const asteroid5 = new Asteroid(images[4]);
-const asteroid6 = new Asteroid(images[5]);
-const asteroid7 = new Asteroid(images[6]);
-const asteroid8 = new Asteroid(images[7]);
-const asteroid9 = new Asteroid(images[8]);
-const asteroid10 = new Asteroid(images[9]);
+// const asteroid2 = new Asteroid(images[1]);
+// const asteroid3 = new Asteroid(images[2]);
+// const asteroid4 = new Asteroid(images[3]);
+// const asteroid5 = new Asteroid(images[4]);
+// const asteroid6 = new Asteroid(images[5]);
+// const asteroid7 = new Asteroid(images[6]);
+// const asteroid8 = new Asteroid(images[7]);
+// const asteroid9 = new Asteroid(images[8]);
+// const asteroid10 = new Asteroid(images[9]);
 
 // calling drop method to start dropping asteroids
 let timeB4drop = 1000;
 async function dropAsteroids() {
   await asteroid1.drop(timeB4drop);
-  await asteroid2.drop(timeB4drop);
-  await asteroid3.drop(timeB4drop);
-  await asteroid4.drop(timeB4drop);
-  await asteroid5.drop(timeB4drop);
-  await asteroid6.drop(timeB4drop);
-  await asteroid7.drop(timeB4drop);
-  await asteroid8.drop(timeB4drop);
-  await asteroid9.drop(timeB4drop);
-  await asteroid10.drop(timeB4drop);
+  // await asteroid2.drop(timeB4drop);
+  // await asteroid3.drop(timeB4drop);
+  // await asteroid4.drop(timeB4drop);
+  // await asteroid5.drop(timeB4drop);
+  // await asteroid6.drop(timeB4drop);
+  // await asteroid7.drop(timeB4drop);
+  // await asteroid8.drop(timeB4drop);
+  // await asteroid9.drop(timeB4drop);
+  // await asteroid10.drop(timeB4drop);
 }
 dropAsteroids();
 
-// Countdown clock
-let clockDiv = document.getElementById("clock");
-let startTime = 60;
+// // Countdown clock
+// let clockDiv = document.getElementById("clock");
+// let startTime = 60;
 
-const countDown = setInterval(() => {
-  startTime--;
-  displayTime(startTime);
-  if ((startTime <= 0) | (startTime < 1)) {
-    endTime();
-    clearInterval(countDown);
-  }
-}, 1000);
+// const countDown = setInterval(() => {
+//   startTime--;
+//   displayTime(startTime);
+//   if ((startTime <= 0) | (startTime < 1)) {
+//     endTime();
+//     clearInterval(countDown);
+//   }
+// }, 1000);
 
-function displayTime(second) {
-  const min = Math.floor(second / 60);
-  const sec = Math.floor(second % 60);
-  clockDiv.innerHTML = `Time: ${min < 10 ? "0" : ""}${min}:${
-    sec < 10 ? "0" : ""
-  }${sec}`;
-}
+// function displayTime(second) {
+//   const min = Math.floor(second / 60);
+//   const sec = Math.floor(second % 60);
+//   clockDiv.innerHTML = `Time: ${min < 10 ? "0" : ""}${min}:${
+//     sec < 10 ? "0" : ""
+//   }${sec}`;
+// }
 
-function endTime() {
-  clockDiv.innerHTML = "Level up";
-}
+// function endTime() {
+//   clockDiv.innerHTML = "Level up";
+// }
--- newAsteroid.js
+++ newAsteroid.js
@@ -22,14 +22,18 @@ class Asteroid {
     await this.sleep(time);
     const object = document.getElementById(this.id);
 
+    // extract numerical part of ID to set div drop lane
     const objectNum = this.id.replace(/^\D+/g, "");
     const objLeft = objectNum * 100;
     object.style.left = `${objLeft}px`;
 
+    // style to show single images
     object.style.display = "inline-block";
     object.style.whiteSpace = "nowrap";
     object.style.overflow = "hidden";
+    object.style.backgroundRepeat = "no-repeat";
 
+    // additional styling
     const divSize = 100;
     object.style.width = `${divSize}px`;
     object.style.height = `${divSize}px`;
@@ -38,27 +42,31 @@ class Asteroid {
     object.style.top = "0px";
     object.style.padding = 0;
     object.style.borderWidth = 0;
-    object.style.backgroundRepeat = "no-repeat";
 
     // start of collision detection
     const asteroidRect = object.getBoundingClientRect();
 
-    // first, initialize top and bottom values of asteroid
+    // first, initialize top and bottom values of
+    // asteroid before kicking off setInterval as Top and Bottom
+    // values will have to be updated in there
     let asteroidTop = 0;
     let asteroidBottom = 100;
     let asteroidLeft = asteroidRect.left;
     let asteroidRight = asteroidRect.right;
 
     setInterval(() => {
-      // the update asteroid's vertical position
-      asteroidTop += 1;
-      asteroidBottom += 1;
-
       // obtaining current position of spaceship
       let ssRect = document.getElementById("ss").getBoundingClientRect();
 
+      // updating asteroid's vertical position informationg
+      asteroidTop += 1;
+      asteroidBottom += 1;
       object.style.top = `${asteroidTop}px`;
 
+      // storing collision detection flag
+      // BUG: For some reason the top and bottom values obtained in ssRect
+      // are not consistent with the image display, hence the subtraction
+      // of 100 below
       let collisionFlag =
         asteroidRight > ssRect.left &&
         asteroidLeft < ssRect.right &&
@@ -66,28 +74,58 @@ class Asteroid {
         asteroidTop < ssRect.bottom - 100;
 
       if (collisionFlag) {
+        // creating table arrays for debugging collisions
         const arrayDebug = [
           [this.id],
           ["asteroidTop", "asteroidBottom", "ssRect.top", "ssRect.bottom"],
           [asteroidTop, asteroidBottom, ssRect.top, ssRect.bottom],
         ];
         console.table(arrayDebug);
-        const arrayDebug3 = [
+        const arrayDebug2 = [
           [this.id],
           ["asteroidLeft", "asteroidRight", "ssRect.left", "ssRect.right"],
           [asteroidLeft, asteroidRight, ssRect.left, ssRect.right],
         ];
-        console.table(arrayDebug3);
+        console.table(arrayDebug2);
+
         alert("Game Over");
         // object.style.top = "0px";
         // window.location.reload();
       }
 
+      // resetting top and bottom positions to avoid infinitely dropping asteroids
       if (asteroidTop >= 1000) {
         asteroidTop = 0;
         asteroidBottom = 100;
       }
     }, 1);
+  }
+
+  // Countdown clock
+  startTimer() {
+    let clockDiv = document.getElementById("clock");
+    let startTime = 60;
+
+    const countDown = setInterval(() => {
+      startTime--;
+      displayTime(startTime);
+      if ((startTime <= 0) | (startTime < 1)) {
+        endTime();
+        clearInterval(countDown);
+      }
+    }, 1000);
+
+    function displayTime(second) {
+      const min = Math.floor(second / 60);
+      const sec = Math.floor(second % 60);
+      clockDiv.innerHTML = `Time: ${min < 10 ? "0" : ""}${min}:${
+        sec < 10 ? "0" : ""
+      }${sec}`;
+    }
+
+    function endTime() {
+      clockDiv.innerHTML = "Level up";
+    }
   }
 
   sleep(time) {
